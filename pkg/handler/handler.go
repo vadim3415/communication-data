9f1d95a203cac302ee11991b869e42a2c79e38a4
vadim 3415 <vadim3415@gmail.com>
2022-02-25 00:34:46 +0300

    status code

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -13,41 +13,45 @@ import (
 
 var mmsJsonSlice []model.MMSData
 var resultSlice []model.MMSData
+var nilSlice []model.MMSData
 
 func getMMS(c *gin.Context) {
-
-	resp, err := http.Get("http://127.0.0.1:8383/mms")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, mmsJsonSlice)
-		logrus.Println(err)
-		return
-	}
-
-	textBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, mmsJsonSlice)
-		logrus.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if err := json.Unmarshal(textBytes, &mmsJsonSlice); err != nil {
-		c.JSON(http.StatusBadRequest, mmsJsonSlice)
-		logrus.Println(err)
-		return
-	}
-
-	for _, v := range mmsJsonSlice {
-		checkCountry := processingData.CheckCountryFunc(v.Country)
-		checkProvider := processingData.CheckProviderFunc(v.Provider)
-
-		if len(checkCountry) > 0 && len(checkProvider) > 0 {
-			resultSlice = append(resultSlice, v)
+	statusCode := c.Writer.Status()
+	if statusCode == 200 {
+		resp, err := http.Get("http://127.0.0.1:8383/mms")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, nilSlice)
+			logrus.Println(err)
+			return
 		}
+
+		textBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, nilSlice)
+			logrus.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		if err := json.Unmarshal(textBytes, &mmsJsonSlice); err != nil {
+			c.JSON(http.StatusBadRequest, mmsJsonSlice)
+			logrus.Println(err)
+			return
+		}
+
+		for _, v := range mmsJsonSlice {
+			checkCountry := processingData.CheckCountryFunc(v.Country)
+			checkProvider := processingData.CheckProviderFunc(v.Provider)
+
+			if len(checkCountry) > 0 && len(checkProvider) > 0 {
+				resultSlice = append(resultSlice, v)
+			}
+		}
+		fmt.Println(resultSlice)
+		c.JSON(http.StatusOK, resultSlice)
+		resultSlice = nil
+		mmsJsonSlice = nil
+		return
 	}
-	fmt.Println(resultSlice)
-	c.JSON(http.StatusOK, resultSlice)
-	resultSlice = nil
-	mmsJsonSlice = nil
-	return
+	c.JSON(http.StatusOK, nilSlice)
 }

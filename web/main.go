d54d2260f12d9caeca391b60119eec6bbfdef2e2
vadim 3415 <vadim3415@gmail.com>
2022-03-16 02:22:21 +0300

    procfile4

--- web/main.go
+++ web/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
 	"strconv"
 	"strings"
-	"io/ioutil"
-	"fmt"
 	"time"
-	"encoding/json"
-	"net/http"
-	"github.com/gorilla/mux"
 )
 
 const minResponseTime = 30
@@ -55,19 +55,19 @@ var SupportCollection []SupportItem
 var AccendentCollection []AccendentItem
 
 type MMSItem struct {
-	Country  string `json:"country"`
-	Provider string `json:"provider"`
-	Bandwidth string `json:"bandwidth"`
+	Country      string `json:"country"`
+	Provider     string `json:"provider"`
+	Bandwidth    string `json:"bandwidth"`
 	ResponseTime string `json:"response_time"`
 }
 
 type SupportItem struct {
-	Topic string `json:"topic"`
-	ActiveTickets int `json:"active_tickets"`
+	Topic         string `json:"topic"`
+	ActiveTickets int    `json:"active_tickets"`
 }
 
 type AccendentItem struct {
-	Topic string  `json:"topic"`
+	Topic  string `json:"topic"`
 	Status string `json:"status"`
 }
 
@@ -84,27 +84,26 @@ var AccendentTopics = []string{
 	"API Slow latency",
 }
 
-
 func init() {
 	rand.Seed(time.Now().UnixNano())
 
 	firstSMSRowForCorrupt = rand.Intn(70)
-	fmt.Printf("First SMS row for currupt %d\n", firstSMSRowForCorrupt + 1)
+	fmt.Printf("First SMS row for currupt %d\n", firstSMSRowForCorrupt+1)
 
 	secondSMSRowForCorrupt = rand.Intn(90)
-	fmt.Printf("Second SMS row for currupt %d\n", secondSMSRowForCorrupt + 1)
+	fmt.Printf("Second SMS row for currupt %d\n", secondSMSRowForCorrupt+1)
 
 	firstVoiceRowForCorrupt = rand.Intn(70)
-	fmt.Printf("First Voice row for currupt %d\n", firstVoiceRowForCorrupt + 1)
+	fmt.Printf("First Voice row for currupt %d\n", firstVoiceRowForCorrupt+1)
 
 	secondVoiceRowForCorrupt = rand.Intn(90)
-	fmt.Printf("Second Voice row for currupt %d\n", secondVoiceRowForCorrupt + 1)
+	fmt.Printf("Second Voice row for currupt %d\n", secondVoiceRowForCorrupt+1)
 
 	firstEmailRowForCorrupt = rand.Intn(70)
-	fmt.Printf("First Email row for currupt %d\n", firstEmailRowForCorrupt + 1)
+	fmt.Printf("First Email row for currupt %d\n", firstEmailRowForCorrupt+1)
 
 	secondEmailRowForCorrupt = rand.Intn(90)
-	fmt.Printf("Second Email row for currupt %d\n", secondEmailRowForCorrupt + 1)
+	fmt.Printf("Second Email row for currupt %d\n", secondEmailRowForCorrupt+1)
 }
 
 func main() {
@@ -124,7 +123,7 @@ func main() {
 
 func shuffleSmsData() {
 	var data string
-	for i, country := range(getCountriesList()) {
+	for i, country := range getCountriesList() {
 		row := strings.Join([]string{
 			country,
 			getRandomBandwidthInString(),
@@ -151,13 +150,13 @@ func shuffleSmsData() {
 
 func shuffleMMSData() []MMSItem {
 	data := make([]MMSItem, 0)
-	for _, country := range(getCountriesList()) {
+	for _, country := range getCountriesList() {
 		data = append(
 			data,
 			MMSItem{
-				Country: country,
-				Provider: getMMSProviderByCountry(country),
-				Bandwidth: getRandomBandwidthInString(),
+				Country:      country,
+				Provider:     getMMSProviderByCountry(country),
+				Bandwidth:    getRandomBandwidthInString(),
 				ResponseTime: getRandomResponseTimeInString(),
 			},
 		)
@@ -168,7 +167,7 @@ func shuffleMMSData() []MMSItem {
 
 func shuffleVoiceData() {
 	var data string
-	for i, country := range(getCountriesList()) {
+	for i, country := range getCountriesList() {
 		row := strings.Join([]string{
 			country,
 			getRandomBandwidthInString(),
@@ -406,7 +405,7 @@ func getRandomResponseTimeInString() string {
 func getRandomConnectionStability() string {
 	stability := getRandomIntBetweenValues(minConnectionStability, maxConnectionStability)
 
-	return fmt.Sprintf("%.2f", float32(stability) / 1000)
+	return fmt.Sprintf("%.2f", float32(stability)/1000)
 }
 
 func getRandomTTFB() string {
@@ -426,7 +425,7 @@ func getRandomEmailDeliveryTime() string {
 }
 
 func getRandomIntBetweenValues(min int, max int) int {
-	return rand.Intn(max - min) + min
+	return rand.Intn(max-min) + min
 }
 
 func listenAndServeHTTP() {
@@ -437,7 +436,7 @@ func listenAndServeHTTP() {
 	router.HandleFunc("/accendent", handleAccendent)
 	router.HandleFunc("/test", handleTest).Methods("GET", "OPTIONS")
 
-	http.ListenAndServe("127.0.0.1:8383", router)
+	http.ListenAndServe("0.0.0.0:5000", router)
 }
 
 func handleMMS(w http.ResponseWriter, r *http.Request) {

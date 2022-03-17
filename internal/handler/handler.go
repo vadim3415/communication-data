6358416c0e5c224f0bc02e92b07c4f91fc75ec74
vadim 3415 <vadim3415@gmail.com>
2022-03-17 22:41:10 +0300

    simulator + localhost4

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -22,15 +22,16 @@ func api(c *gin.Context) {
 	resultSet := processingData.GetResultData()
 	billing := resultSet.Billing
 
-	if len(resultSet.SMS) > 0 && len(resultSet.MMS) > 0 && len(resultSet.VoiceCall) > 0 && len(resultSet.Email) > 0 &&
-		len(resultSet.Support) > 0 && len(resultSet.Incidents) > 0 && billing.CheckoutPage == true ||
-		billing.CheckoutPage == false && len(resultSet.Incidents) > 1 {
+	if len(resultSet.SMS) > 0 && len(resultSet.MMS) > 1 && len(resultSet.VoiceCall) > 0 && len(resultSet.Email) > 0 &&
+		len(resultSet.Support) > 1 && len(resultSet.Incidents) > 1 && billing.CheckoutPage == true ||
+		billing.CheckoutPage == false {
 
 		result.Data = resultSet
 		result.Status = true
 	} else {
 		result.Status = false
 		result.Error = "Error on collect data"
+		result.Data = resultSet
 	}
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Headers", "Content-Type")

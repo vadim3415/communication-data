aaf8f4191c8e319ea470adba652f8a1fdc73128a
vadim 3415 <vadim3415@gmail.com>
2022-03-17 21:23:27 +0300

    simulator + localhost

--- data_simulator/main.go
+++ data_simulator/main.go
@@ -436,7 +436,7 @@ func listenAndServeHTTP() {
 	router.HandleFunc("/accendent", handleAccendent)
 	router.HandleFunc("/test", handleTest).Methods("GET", "OPTIONS")
 
-	http.ListenAndServe("127.0.0.1:8383", router)
+	http.ListenAndServe(":8383", router)
 }
 
 func handleMMS(w http.ResponseWriter, r *http.Request) {
